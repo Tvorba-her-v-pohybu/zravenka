15cdca56df4a7a56bb492ab4456e02e359f1d9eb
LindaxCookie <altamira@email.cz>
2025-01-13 16:48:24 +0100

    20250113 optimalizace
    20250113 optimalizace

--- godot_project/character_body_2d.gd
+++ godot_project/character_body_2d.gd
@@ -12,10 +12,10 @@ var probehlo_4 = 0
 var probehlo_5 = 0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * _delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
@@ -27,36 +27,24 @@ func _physics_process(delta: float) -> void:
 
 		
 	var direction := Input.get_axis("ui_left", "ui_right")
-	
+	#Input.get_axis == "ui_left"
+	#	
 		
 	if direction:
-		
 		print (platforma)
-		print ($"../CharacterBody2D".position.x)
-		print ($"../CharacterBody2D".position.y)
-		print (zachyceno)
-		print ($"../Area2D_platform_2".position.x)
-		print ("--------------")
-		
-		#print ($"../CharacterBody2D".position.y)	
-		#print ($"../CharacterBody2D".scale.y)
-		#print ($"../CharacterBody2D".JUMP_VELOCITY)
-		#nove_pocitadlo_zmenseni = nove_pocitadlo_zmenseni + 0.001
+		print (maxik)
 		$"../CharacterBody2D".scale.x=$"../CharacterBody2D".scale.x-nove_pocitadlo_zmenseni
 		$"../CharacterBody2D".scale.y=$"../CharacterBody2D".scale.y-nove_pocitadlo_zmenseni
-		#print ($"../Node2D.pocitadlo_zmenseni")
-		# if $"../CharacterBody2D".scale.x > 1.4:
-		# 	$"../StaticBody2D/CollisionShape2D_P2".set_rotation_degrees(0) # je to prostupné po otočení
-		if $"../CharacterBody2D".scale.x > 1.5 and $"../CharacterBody2D".platforma == 1 and zachyceno == 0:
+		if $"../CharacterBody2D".scale.x > 1.4 and $"../CharacterBody2D".platforma == 1 and zachyceno == 0:
 			$"../StaticBody2D/CollisionShape2D_P2".set_rotation_degrees(0) # je to prostupné po otočení
 			platforma = 2
-		if $"../CharacterBody2D".scale.x > 1.35 and $"../CharacterBody2D".platforma == 2 and zachyceno == 1:
+		if $"../CharacterBody2D".scale.x > 1.4 and $"../CharacterBody2D".platforma == 2 and zachyceno == 1:
 			$"../StaticBody2D/CollisionShape2D_P3".set_rotation_degrees(0) # je to prostupné po otočení
 			platforma = 3	
-		if $"../CharacterBody2D".scale.x > 1.3 and $"../CharacterBody2D".platforma == 3 and zachyceno == 2:
+		if $"../CharacterBody2D".scale.x > 1.4 and $"../CharacterBody2D".platforma == 3 and zachyceno == 2:
 			$"../StaticBody2D/CollisionShape2D_P4".set_rotation_degrees(0) # je to prostupné po otočení
 			platforma = 4	
-		if $"../CharacterBody2D".scale.x > 1.2 and $"../CharacterBody2D".platforma == 4 and zachyceno == 3:
+		if $"../CharacterBody2D".scale.x > 1.4 and $"../CharacterBody2D".platforma == 4 and zachyceno == 3:
 			$"../StaticBody2D/CollisionShape2D_P5".set_rotation_degrees(0) # je to prostupné po otočení
 			platforma = 5	
 
@@ -69,17 +57,7 @@ func _physics_process(delta: float) -> void:
 			
 			if $"../CharacterBody2D".scale.x > 6:
 				$"../CharacterBody2D/zravila_win".visible = true
-			#$"../Area2D_food_01".position.x=500000
-			#$"../Area2D_food_02".position.x=500000
-			#$"../CharacterBody2D".scale.x = 3.5
-			#$"../CharacterBody2D".scale.y = 3.5
-			#$"../CharacterBody2D".JUMP_VELOCITY = -1500
-			#$"../StaticBody2D/CollisionShape2D_P2".set_rotation_degrees(180)
-			#$"../StaticBody2D/CollisionShape2D_P3".set_rotation_degrees(180)
-			#$"../StaticBody2D/CollisionShape2D_P4".set_rotation_degrees(180)
-			#$"../StaticBody2D/CollisionShape2D_P5".set_rotation_degrees(180)
-			
-		#if $"../CharacterBody2D".position.y < 80 and $"../CharacterBody2D".platforma == 2:
+
 		if $"../CharacterBody2D".scale.x < 0.05:
 			get_tree().quit()
 			
@@ -87,39 +65,58 @@ func _physics_process(delta: float) -> void:
 		
 		velocity.x = direction * SPEED
 		
+		
+		# otáčení postavičky
+		if velocity.x < 0 and $Sprite2D.scale.x > 0:
+			$Sprite2D.scale.x *= -1.0
+			
+		if velocity.x > 0 and $Sprite2D.scale.x < 0:
+			$Sprite2D.scale.x *= -1.0
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-
-
-
-
-		#if $"../CharacterBody2D".position.y > 130 and $"../CharacterBody2D".position.y < 131:
-			#var platforma = 3
-			
-		#print ($"../CharacterBody2D".position.y)
-		#  set_one_way_collision(CollisionShape2D_P2: true)
-		#  Node2D.StaticBody2D.CollisionShape2D_P2.set_one_way_collision(CollisionShape2D_P2: true)
 		
 	move_and_slide()
 	
 	var koefik = 175
 	objekty_na_platformach = 240 - koefik * platforma
 	
+	
+var pocitadlo = 0
+var maxik = 0
+	
+func _process(_delta: float) -> void:
+	
+	if zvetsit_plynule == 1:
+		if pocitadlo < maxik:
+			$"../CharacterBody2D".scale=$"../CharacterBody2D".scale * 1.01
+		pocitadlo += 1
+		if pocitadlo >= maxik:
+			zvetsit_plynule = 0
 		
-
+	if zmensit_plynule == 1:
+		if pocitadlo < maxik:
+			$"../CharacterBody2D".scale=$"../CharacterBody2D".scale * 0.95
+		pocitadlo += 1
+		if pocitadlo >= maxik:
+		#if $"../CharacterBody2D".scale.x < 1
+			zmensit_plynule = 0	
+		
+var zvetsit_plynule = 0
+var zmensit_plynule = 0
 func _zobraz_food_1():
 		$"../Area2D_food_01".position.x=randf_range(-800, 150)
 		$"../Area2D_food_01".position.y=randf_range(objekty_na_platformach, objekty_na_platformach-80)
-		$"../CharacterBody2D".scale.x=$"../CharacterBody2D".scale.x * 1.1
-		$"../CharacterBody2D".scale.y=$"../CharacterBody2D".scale.y * 1.1
-		
+		zvetsit_plynule = 1
+		pocitadlo = 0
+		maxik = 10
+
 func _zobraz_food_2():
 		$"../Area2D_food_02".position.x=randf_range(-800, 150)
 		$"../Area2D_food_02".position.y=randf_range(objekty_na_platformach, objekty_na_platformach-80)
-		$"../CharacterBody2D".scale.x=$"../CharacterBody2D".scale.x * 1.1
-		$"../CharacterBody2D".scale.y=$"../CharacterBody2D".scale.y * 1.1
-
+		zvetsit_plynule = 1
+		pocitadlo = 0
+		maxik = 10
 
 func _on_area_2d_food_01_body_entered(_body: Node2D) -> void:
 	_zobraz_food_1()
@@ -134,8 +131,9 @@ func _on_area_2d_food_02_body_entered(_body: Node2D) -> void:
 func _on_area_2d_platform_2radsinova_body_entered(_body: Node2D) -> void:
 		
 		if probehlo_2 == 0:
-			$"../CharacterBody2D".scale.x = 0.75
-			$"../CharacterBody2D".scale.y = 0.75
+			zmensit_plynule = 1
+			pocitadlo = 0
+			maxik = 10
 			probehlo_2 = 1
 		zachyceno = 1
 		_zobraz_food_1()
@@ -147,8 +145,9 @@ func _on_area_2d_platform_2radsinova_body_entered(_body: Node2D) -> void:
 func _on_area_2d_platform_3_body_entered(_body: Node2D) -> void:
 		
 		if probehlo_3 == 0:
-			$"../CharacterBody2D".scale.x = 0.7
-			$"../CharacterBody2D".scale.y = 0.7
+			zmensit_plynule = 1
+			pocitadlo = 0
+			maxik = 15
 			probehlo_3 = 1
 		zachyceno = 2	
 		_zobraz_food_1()
@@ -158,8 +157,9 @@ func _on_area_2d_platform_3_body_entered(_body: Node2D) -> void:
 func _on_area_2d_platform_4_body_entered(_body: Node2D) -> void:
 		
 		if probehlo_4 == 0:
-			$"../CharacterBody2D".scale.x = 0.6
-			$"../CharacterBody2D".scale.y = 0.6
+			zmensit_plynule = 1
+			pocitadlo = 0
+			maxik = 20
 			probehlo_4 = 1
 		zachyceno = 3
 		_zobraz_food_1()
@@ -169,8 +169,9 @@ func _on_area_2d_platform_4_body_entered(_body: Node2D) -> void:
 func _on_area_2d_platform_5_body_entered(_body: Node2D) -> void:
 		
 		if probehlo_5 == 0:
-			$"../CharacterBody2D".scale.x = 0.55
-			$"../CharacterBody2D".scale.y = 0.55
+			zmensit_plynule = 1
+			pocitadlo = 0
+			maxik = 25
 			probehlo_5 = 1
 		zachyceno = 4
 		_zobraz_food_1()
